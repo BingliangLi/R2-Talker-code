1a893c680e8e9a2a15cb43bb4ed87c78c3a48d75
李炳良 <11162248@vivo.com>
2024-01-15 19:43:49 +0800

    nikki

--- binarizedFile2landmarks.py
+++ binarizedFile2landmarks.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-data = np.load("./GeneFace/data/binary/videos/Obama/trainval_dataset.npy", allow_pickle=True).tolist()
+data = np.load("../GeneFace/data/binary/videos/nikki/trainval_dataset.npy", allow_pickle=True).tolist()
 print(data.keys())
 train_samples = data['train_samples']
 val_samples = data['val_samples']
@@ -17,6 +17,6 @@ for sample in val_samples:
     val_lm3d.append(sample['idexp_lm3d'])
     lm3d.append(sample['idexp_lm3d'])
 
-np.save('./data/Obama/aud_idexp_train.npy', np.array(train_lm3d))
-np.save('./data/Obama/aud_idexp_val.npy', np.array(val_lm3d))
-np.save('./data/Obama/aud_idexp.npy', np.array(lm3d))
+np.save('./data/nikki/aud_idexp_train.npy', np.array(train_lm3d))
+np.save('./data/nikki/aud_idexp_val.npy', np.array(val_lm3d))
+np.save('./data/nikki/aud_idexp.npy', np.array(lm3d))
